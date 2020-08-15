d3d579961d843df32c3a31384b00847f0f0ed4e2
Danilo <danilo_as1992@hotmail.com>
2020-08-15 15:35:25 -0300

    RecyclerView Parte-2

--- RecyclerView/app/src/main/java/com/teste/consultar/recyclerview/src/activity/MainActivity.java
+++ RecyclerView/app/src/main/java/com/teste/consultar/recyclerview/src/activity/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.teste.consultar.recyclerview.R;
 import com.teste.consultar.recyclerview.src.adapter.MyAdapter;
+import com.teste.consultar.recyclerview.src.listener.RecyclerItemClickListener;
 import com.teste.consultar.recyclerview.src.model.Filme;
 
 import java.util.ArrayList;
@@ -30,11 +34,36 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter adapter = new MyAdapter(listaFilmes);
 
-
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true); // Melhora performance
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                Toast.makeText(getApplicationContext(), filme.getTitulo(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                final String mensagem = String.format("%s (%s) - %s  ",  filme.getTitulo(), filme.getAno(), filme.getGenero());
+                                Toast.makeText(getApplicationContext(), mensagem, Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
     }
 }
