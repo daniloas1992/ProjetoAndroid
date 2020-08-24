d2dbf09b05a72b60394bcb1aa7f34bcf1857cddc
Danilo <danilo_as1992@hotmail.com>
2020-08-23 22:22:53 -0300

    Projeto Firebase

--- FirebaseApp/app/src/main/java/com/teste/consultar/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/com/teste/consultar/firebaseapp/MainActivity.java
@@ -3,10 +3,19 @@ package com.teste.consultar.firebaseapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,8 +25,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageView;
+    private Button btnEnviar;
+    private Button btnDeletar;
+    private Button btnDownload;
 
     private DatabaseReference dbReference = FirebaseDatabase.getInstance().getReference(); // getReference para começar no primeiro nó do banco
     private FirebaseAuth userAuth = FirebaseAuth.getInstance();
@@ -26,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imgCelular);
+        btnEnviar = findViewById(R.id.btnEnviar);
+        btnDeletar = findViewById(R.id.btnDeletar);
+        btnDownload = findViewById(R.id.btnDownload);
+
+        btnEnviar.setOnClickListener(this);
+        btnDeletar.setOnClickListener(this);
+        btnDownload.setOnClickListener(this);
 
         // Criando usuário para acessar o banco do Firebase:
         /*userAuth.createUserWithEmailAndPassword("daniloas1992@gmail.com","1234!45@").addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
@@ -134,6 +163,121 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnEnviar:
+                fazerUploadImagem();
+                break;
+
+
+            case R.id.btnDeletar:
+                deletarImagem();
+                break;
+
+            case R.id.btnDownload:
+                downloadImagem();
+                break;
+        }
+    }
+
+    private void fazerUploadImagem() {
+
+        // Carrega a imagem em memória
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+
+        // Recupeera o bitmap da imagem
+        Bitmap bitmap = imageView.getDrawingCache();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        //Comprime a imagem
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 75, baos);
+
+        //Converte o baos para pixels em uma matriz de bytes (Formato esperado pelo firebase)
+        byte[] dadosImagem = baos.toByteArray();
+
+        //Define nós para o storage
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+        //Cria um diretório "imagens"
+        StorageReference imagens = storageReference.child("imagens");
+
+        //Gera um nome único para a imagem
+        String nomeArquivo = UUID.randomUUID().toString();
+
+        //Cria a referência para a imagem que será enviada para o diretório "imagens"
+        final StorageReference imagemRef = imagens.child(String.format("%s.jpeg",nomeArquivo));
+
+        UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+
+        uploadTask.addOnFailureListener(MainActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Upload da Imagem falhou: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        })
+        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                imagemRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        Uri url = task.getResult();
+                        Toast.makeText(MainActivity.this, "Upload da Imagem concluído: " + url.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
     }
+
+    private void deletarImagem() {
+
+        //Define nós para o storage
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+        //Define a referência da imagem dentro do diretório "imagens". Se o diretório não existir vai ser criado.
+        StorageReference imagens = storageReference.child("imagens");
+
+        //Cria a referência para a imagem que será deletada
+        StorageReference imagemRef = imagens.child("celular.jpeg");
+
+        imagemRef.delete().addOnFailureListener(MainActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Erro ao deletar imagem: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        })
+        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Void>() {
+             @Override
+             public void onSuccess(Void aVoid) {
+                 Toast.makeText(MainActivity.this, "Imagem deletada com sucesso!", Toast.LENGTH_SHORT).show();
+             }
+         });
+    }
+
+    private void downloadImagem() {
+
+        /*
+        *  DOCUMENTAÇÃO DISPONÍVEL EM: https://github.com/firebase/FirebaseUI-Android/tree/master/storage
+        * */
+
+        //Define nós para o storage
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+        //Define a referência da imagem dentro do diretório "imagens". Se o diretório não existir vai ser criado.
+        StorageReference imagens = storageReference.child("imagens");
+
+        //Cria a referência para a imagem que será deletada
+        StorageReference imagemRef = imagens.child("961776f6-5cc2-415f-a793-aee80159ee38.jpeg");
+
+        GlideApp.with(MainActivity.this).load(imagemRef).into(imageView);
+
+    }
+
 }
